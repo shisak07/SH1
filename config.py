00f26f6bddb0a0f36bafc68d063d03b5f95345ea
nafishalam843438 <uniqueshorts580@gmail.com>
2024-12-31 13:19:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8066216378:AAH20METU25McUevqtsKaFiSKCuGI0H5zR8"
+ADMIN_IDS = ["5579438195"]
+GROUP_ID = -1002442804421  # Replace with your group ID
+GROUP_LINK = "https://t.me/+JbLz_yuKp4JiMGU1"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
